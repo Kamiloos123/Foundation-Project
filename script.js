119899e22a065a75a9b0edbedec7877854e5723b
Kamil <ddmw33@durham.ac.uk>
2023-01-23 13:52:20 +0000

    Questionnaire template

--- script.js
+++ script.js
@@ -3,20 +3,22 @@ const form = document.getElementById('questionnaire');
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
+  // Get the values of the form and start new
   location.href='questionnaire.html';
+  
   let score = 0;
 
   // Validate the form and calculate the score
-  const question1 = form.question1.value;
-  if (question1 === 'red') {
-    score++;
-  }
-  const question2 = form.question2.value;
-  if (question2 === 'cat') {
-    score++;
-  }
+//   const question1 = form.questionGender.value;
+//   if (question1 === 'red') {
+//     score++;
+//   }
+//   const question2 = form.questionAge.value;
+//   if (question2 === 'cat') {
+//     score++;
+//   }
 
-  // Display the results
-  alert(`You scored ${score} out of 2`);
+//   // Display the results
+//   alert(`You scored ${score} out of 2`);
 });
 
