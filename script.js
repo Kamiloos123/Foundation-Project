f99d1107e4d1b44bf90997ffd6e2dd892abb39ac
Kamil <kamilgrec1@googlemail.com>
2023-02-06 13:57:59 +0000

    Pop up created

--- script.js
+++ script.js
@@ -21,4 +21,42 @@ form.addEventListener('submit', (event) => {
 //   // Display the results
 //   alert(`You scored ${score} out of 2`);
 });
+// Get the modal
+var modal = document.getElementById("myModal");
 
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// Get the "Yes" button
+var yesBtn = document.getElementById("yesBtn");
+
+// Get the "No" button
+var noBtn = document.getElementById("noBtn");
+
+// When the page loads, open the modal
+window.onload = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on the "Yes" button
+yesBtn.onclick = function() {
+  // Do something here, such as redirecting the user to another page
+  window.location.href = "https://www.example.com";
+}
+
+// When the user clicks on the "No" button
+noBtn.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
