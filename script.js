e38e229e0ac6d3c69febe444a7aa48e12507a65e
Kamil <kamilgrec1@googlemail.com>
2023-02-20 18:44:16 +0000

    Please fix js

--- script.js
+++ script.js
@@ -1,13 +1,49 @@
-// JavaScript for validating the questionnaire and displaying the results
+// JavaScript for validating the questionnaire and sending them to a database
 const form = document.getElementById('questionnaire');
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  // Get the values of the form and start new
-  location.href='questionnaire.html';
-  
+const mysql = require('mysql2/promise');
 
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const connection = await mysql.createConnection({
+    host: 'kamil-foundation-server.mysql.database.azure.com',
+    user: 'admin1',
+    password: 'Waser567765.',
+    database: 'project',
+    port: 3306,
+    ssl: {
+      ca: fs.readFileSync('DigiCertTLSRSA4096RootG5.crt'),
+    },
+  });
+
+  try {
+    const answers = document.querySelectorAll('input[type="radio"]:checked');
+    const answersArray = Array.from(answers);
+
+    if (answersArray.length < 5) {
+      alert('Please answer all questions');
+    } else {
+      // Insert answers into the database
+      for (let i = 0; i < answersArray.length; i++) {
+        const answer = answersArray[i].value;
+        await connection.query('INSERT INTO demographics VALUES (?)', [answer]);
+      }
+      
+      alert('Thank you for your answers');
+      // Get the values of the form and start new
+      location.href = 'questionnaire.html';
+    }
+  } catch (err) {
+    console.error(err);
+    alert('An error occurred while submitting your answers. Please try again later.');
+  } finally {
+    connection.end();
+  }
 });
+
+
+
 // Get the modal
 var modal = document.getElementById("myModal");
 
