dd475f6997ea2d71ff24e865d9ba850d94b090c5
Kamil <kamilgrec1@googlemail.com>
2023-02-23 00:32:51 +0000

    vcv

---
+++ Scripting.js
@@ -0,0 +1,42 @@
+const form = document.getElementById('Answers');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const answer1 = document.querySelector('input[name="questionPrivacy"]:checked').value;
+  const answer2 = document.querySelector('input[name="questionAvoid"]:checked').value;
+  const answer3 = document.querySelector('input[name="questionCybersecurity"]:checked').value;
+  const answer4 = document.querySelector('input[name="questionResponsiblePrivacy"]:checked').value;
+  const answer5 = document.querySelector('input[name="questionInfo"]:checked').value;
+  const answer6 = document.querySelector('input[name="questionFreq"]:checked').value;
+  const answer7 = document.querySelector('input[name="questionAccept"]:checked').value;
+  const answer8 = document.querySelector('input[name="questionDescribe"]:checked').value;
+  const answer9 = document.querySelector('input[name="QuestionOpen]"').value;
+  
+
+  // EmailJS API
+  (function() {
+    emailjs.init("v4LdSJqfieVrBLaXu");
+  })();
+  
+  const templateParams = {
+    answer1: answer1,
+    answer2: answer2,
+    answer3: answer3,
+    answer4: answer4,
+    answer5: answer5,
+    answer6: answer6,
+    answer7: answer7,
+    answer8: answer8,
+    answer9: answer9
+  };
+  
+  emailjs.send('service_tnkzhnv', 'template_sg6471a', templateParams)
+    .then(function(response) {
+       alert("Thank you for your answers. You can close this page now.");
+       location.href = 'questionnaire.html';
+    }, function(error) {
+       console.error(error);
+       alert('An error occurred while submitting your answers. Please try again later.');
+    });
+});
